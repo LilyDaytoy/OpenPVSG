fece854f83b36f8a3f11795369bffd6f8dbc81fd
LilyDaytoy <C200218@e.ntu.edu.sg>
2022-11-04 17:54:52 +0800

    update pvsg_v1 config

--- configs/_base_/datasets/pvsg_image_panoptic.py
+++ configs/_base_/datasets/pvsg_image_panoptic.py
@@ -1,5 +1,5 @@
 dataset_type = 'PVSGImageDataset'
-data_root = './data/pvsg_demo'
+data_root = './data/pvsg_v1'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=False
